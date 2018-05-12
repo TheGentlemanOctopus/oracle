e1f8635964876d716643e16c035b371395b574f1
Aaron Fishman <uk.afishman@gmail.com>
2018-05-12 15:40:32 +0100

    update framework

--- device.py
+++ device.py
@@ -3,13 +3,14 @@ from multiprocessing import Queue
 
 class Device(object):
     """Generic Device"""
-    def __init__(self):
+    def __init__(self, frame_rate=30):
         self.out_queue = Queue()
+        self.frame_rate = frame_rate
 
     def main(self):
         while True:
-            self.put(time.time())
-            time.sleep(1.0/30)
+            self.put(self.update())
+            time.sleep(1.0/self.frame_rate)
 
     def put(self, data):
         # TODO: Make clear function with the following...
@@ -17,6 +18,9 @@ class Device(object):
             self.out_queue.get()
 
         self.out_queue.put(data)
+
+    def update(self):
+        raise NotImplementedError("pls define update")
 
 if __name__ == '__main__':
     device = Device()
