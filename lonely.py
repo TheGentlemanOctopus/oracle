05c080d3e1d98885828df6cd335b4a57475861c6
Aaron Fishman <uk.afishman@gmail.com>
2018-05-12 17:20:51 +0100

    simple pattern for lonely

--- lonely.py
+++ lonely.py
@@ -1,12 +1,19 @@
 from device import Device 
 from pixel import Pixel
+import time
+import numpy as np
 
 class Lonely(Device):
     """Lonely pixel :("""
 
     def setup(self):
-        self.pixels = [Pixel([0,0,0], color=[0, 100, 255])]
+        self.pixels = [Pixel([0,0,0])]
 
     def update(self):
+        t = time.time()
+        self.pixels[0].r = (255.0/2)*(1+np.sin(t))
+        self.pixels[0].g = (255.0/2)*np.cos(t)
+        self.pixels[0].b = 100
+
         return self.pixels
 
--- pixel.py
+++ pixel.py
@@ -1,5 +1,17 @@
 class Pixel(object):
     """docstring for Pixel"""
-    def __init__(self, location, color=None):
+    def __init__(self, location, r=0, g=0, b=0):
         self.location = tuple(location)
-        self.color = color if color else [0,0,0]
+
+        self.r = r
+        self.g = g
+        self.b = b
+
+    @property
+    def color(self):
+        return (self.r, self.g, self.b)
+
+if __name__ == "__main__":
+    pixel = Pixel([0,0,0])
+
+    print pixel.color
