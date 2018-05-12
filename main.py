2b6144154cfcc148e842e86fb3cdd86beac7b91c
Benjamin Winstone <voidnoise@gmail.com>
2018-05-12 17:24:03 +0100

    Trello:SceneDescriptor.json - adding more structure to project regarding this task

--- main.py
+++ main.py
@@ -1,30 +1,15 @@
 import json
 import sys
+from core.utilities import process_descriptor 
 
-def read_json(path):
-    with open(path, 'r') as handle:
-        parsed = json.load(handle)
-    return parsed
 
-def print_json(jObject):
-    jstr = json.dumps(jObject, indent=4, sort_keys=True)
-    print jstr
-    return jstr
 
 if __name__ == '__main__':
 
 	print sys.argv[1]
 
-	scene_descriptor = read_json(sys.argv[1])
+	scene_descriptor = process_descriptor.read_json(sys.argv[1])
 
-	# print_json(scene_descriptor)
+	process_descriptor.print_element(scene_descriptor,'InputDevices')
+	process_descriptor.print_element(scene_descriptor,'OutputDevices')
 
-	input_devices = scene_descriptor['InputDevices']
-	for key in input_devices.keys():
-		print '\n', key
-		print_json(input_devices[key])
-
-	output_devices = scene_descriptor['OutputDevices']
-	for key in output_devices.keys():
-		print '\n', key
-		print_json(output_devices[key])
