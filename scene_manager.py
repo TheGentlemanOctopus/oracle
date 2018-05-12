1a36e00a3c99eeb1090270e1bfbf9146fb2938f3
Aaron Fishman <uk.afishman@gmail.com>
2018-05-12 17:08:44 +0100

    sending pixel colors over opc

--- scene_manager.py
+++ scene_manager.py
@@ -2,19 +2,24 @@ from multiprocessing import Process, Queue
 from lonely import Lonely
 import device
 import time
-
+import opc
 
 class SceneManager(object):
     """docstring for SceneManager"""
-    def __init__(self):
-        pass
+    def __init__(self,  opc_host="127.0.0.1", opc_port=7890):
+        opc_ip = opc_host + ":" + str(opc_port)
+        self.client = opc.Client(opc_ip)
+
+        # If the client is not connected the put_pixels operation is slow as it tries to reconnect
+        if not self.client.can_connect():
+            raise Exception("Could not connect to opc at " + opc_ip)
 
     def start(self):
         d = Lonely()
         p = Process(target=d.main)
         p.start()
         while True:
-            print("Scene ", d.out_queue.get())
+            self.client.put_pixels(d.out_queue.get(), channel=0)
 
 
 if __name__ == '__main__':
