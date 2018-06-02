9ac62646703e4acc3ff5a61e37687177da764971
Aaron Fishman <uk.afishman@gmail.com>
2018-06-02 14:19:44 +0100

    offsetting strips

--- core/devices/big_cube.py
+++ core/devices/big_cube.py
@@ -8,7 +8,7 @@ class BigCube(Device):
         Big daddy cube
     """
 
-    def __init__(self, channel, spacing):
+    def __init__(self, channel, led_spacing, strip_spacing):
         super(BigCube, self).__init__()
 
         # The vertices of the cube
@@ -21,7 +21,7 @@ class BigCube(Device):
             (1, 1, 1),
             (-1, 1, 1),
             (-1, -1, 1)
-        ])
+        ]).astype(float)
 
         # TODO: Set cube length and orientation
         edge_length = 2
@@ -31,15 +31,21 @@ class BigCube(Device):
         route = np.array([(0, 1), (1, 0), (0, 2), (2, 0), (0, 4), (4, 5), (5, 1), (1, 3), (3, 1), (1, 5), (5, 4), (4, 7), (7, 2), (2, 3), (3, 2), (2, 7), (7, 6), (6, 3), (3, 6), (6, 5), (5, 6), (6, 7), (7, 4), (4, 0)])
 
         # Create strips
-        pixels_per_edge = int(float(edge_length)/spacing)
+        pixels_per_edge = int(float(edge_length)/led_spacing)
         self.strips = []
         for path in route:
             start = vertices[path[0]]
             end = vertices[path[1]]
 
+            # A hacky(ish) way to offset strips so they are not on top of each other
+            sign = 1 if path[0] > path[1] else -1
+            offset = strip_spacing*sign
+            start += offset
+            end += offset
+
             direction = end - start
 
-            self.strips.append(Strip(start, direction, spacing, pixels_per_edge))
+            self.strips.append(Strip(start, direction, led_spacing, pixels_per_edge))
 
 
         # Form pixels by channel dict
