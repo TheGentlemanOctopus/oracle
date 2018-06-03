a7a0e4de744a188b0228d2baf0c63f1e670abb60
Ben Holden <benholden7@gmail.com>
2018-06-03 22:01:39 +0100

    Fix panel weirdness

--- core/layouts/panel.py
+++ core/layouts/panel.py
@@ -21,8 +21,11 @@ class Panel:
 
 
             if(sign == -1):
-                strip_translate = np.array([(num_pixels_x * led_spacing), (i * strip_spacing), 0])
+                strip_translate = np.array([((num_pixels_x -1)  * led_spacing), (i * strip_spacing), 0])
+
             else:
                 strip_translate = np.array([0, (i * strip_spacing), 0])
+
             start = np.array(origin) + strip_translate  
+            print start
             self.strips.append(Strip(start, direction, led_spacing, num_pixels_x))
