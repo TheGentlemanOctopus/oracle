58a9bd529592468ba8b9c790cdb981dbebcd16a4
Aaron Fishman <uk.afishman@gmail.com>
2018-06-15 00:43:24 +0100

    setting out_queue for fft_device

--- core/devices/fft_device.py
+++ core/devices/fft_device.py
@@ -2,7 +2,14 @@ from input_device import InputDevice
 from core.udp.fft_server import FftServer
 
 class FftDevice(InputDevice):
+    """
+        A wrapper around the hack fft server
+        TODO: merge fft_server into this
+    """
     def main(self, **fft_server_kwargs):
         fft_server = FftServer(**fft_server_kwargs)
+
+        self.out_queue = fft_server.fft_queue
+
         fft_server.start()
 
