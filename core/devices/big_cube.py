d15307337ed114572b4414c053a0b20a1943e0ac
Aaron Fishman <uk.afishman@gmail.com>
2018-06-17 13:45:15 +0100

    big cube layout

--- core/devices/big_cube.py
+++ core/devices/big_cube.py
@@ -5,50 +5,15 @@ from core.layouts.strip import Strip
 import colorsys
 import time
 
-class BigCube(OutputDevice):
-    """
-        Big daddy cube
-    """
+from core.layouts.big_cube import BigCube
+
+class BigCubeDevice(OutputDevice):
+
 
     def __init__(self, channel, led_spacing, strip_spacing):
         super(BigCube, self).__init__()
 
-        # The vertices of the cube
-        vertices = np.array([
-            (1, -1, -1),
-            (1, 1, -1),
-            (-1, -1, -1),
-            (-1, 1, -1),
-            (1, -1, 1),
-            (1, 1, 1),
-            (-1, 1, 1),
-            (-1, -1, 1)
-        ]).astype(float)
-
-        # TODO: Set cube length and orientation
-        edge_length = 2
-
-        # An eulerian tour around a cube with two strips per edge
-        # Elements are from-to tuples that map to indices in the vertices array
-        route = np.array([(0, 1), (1, 0), (0, 2), (2, 0), (0, 4), (4, 5), (5, 1), (1, 3), (3, 1), (1, 5), (5, 4), (4, 7), (7, 2), (2, 3), (3, 2), (2, 7), (7, 6), (6, 3), (3, 6), (6, 5), (5, 6), (6, 7), (7, 4), (4, 0)])
-
-        # Create strips in the order they should be laid
-        pixels_per_edge = int(float(edge_length)/led_spacing)
-        self.strips = []
-        for path in route:
-            start = vertices[path[0]]
-            end = vertices[path[1]]
-
-            # A hacky(ish) way to offset strips so they are not on top of each other
-            sign = 1 if path[0] > path[1] else -1
-            offset = 0.5*strip_spacing*sign
-            start += offset
-            end += offset
-
-            direction = end - start
-
-            self.strips.append(Strip(start, direction, led_spacing, pixels_per_edge))
-
+        self.big_cube = BigCube(led_spacing, strip_spacing)
 
         # Form pixels by channel dict
         self.pixels_by_channel = {
@@ -61,7 +26,7 @@ class BigCube(OutputDevice):
         """
             Returned in the order that strips should be laid
         """
-        return np.concatenate([strip.pixels for strip in self.strips])
+        return self.big_cube.pixels
     
     def update(self, period=5, hue_range=0.2):
         # Period is the number of seconds it takes a color to lap the cube
