cea0a1e87cb204762152cd01f8451f81dba436fa
Aaron Fishman <uk.afishman@gmail.com>
2018-06-19 18:50:46 +0100

    template

--- core/devices/app/app_server.py
+++ core/devices/app/app_server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from multiprocessing import Queue
 from core.devices.input_device import InputDevice
 
@@ -9,7 +9,10 @@ def run(host, port, output_devices):
     app.data["output_devices"] = output_devices 
     app.run(host=host, port=port)
 
+
 @app.route('/')
 def index():
-    return "".join([device.name for device in app.data["output_devices"]])
+    device_names = [device.name for device in app.data["output_devices"]]
+
+    return render_template('index.html', device_names=device_names)
 
