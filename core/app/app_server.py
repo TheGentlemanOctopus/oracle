d50fcd03f1fe65e83a3359e80dfddebbfbc3cb6d
Aaron Fishman <uk.afishman@gmail.com>
2018-06-20 13:42:22 +0100

    comments

--- core/app/app_server.py
+++ core/app/app_server.py
@@ -5,28 +5,44 @@ from core.devices.input_device import InputDevice
 from core.devices.output_device import switch_animation_message
 
 app = Flask(__name__)
+
+# Simplest way I know share data between functions in flask is to make a data dict
 app.data = {
     "output_devices": {} # key: device name, value: device
 }
 
 def run(host, port, output_devices):
+    """
+        Call this function to start the server
+    """
+    
     app.data["output_devices"] = {device.name: device for device in output_devices} 
     app.run(host=host, port=port)
 
 @app.route('/')
 def index():
+    """
+        Main page of controller
+    """
+
+    # Data for template rendering
     devices = [{
         "name": name,
-        "possible_animations": [a for a in device.possible_animations()]
+        "possible_animations": device.possible_animations()
     } for (name, device) in app.data["output_devices"].items()]
 
     return render_template('index.html', devices=devices)
 
-@app.route('/switch_animation', methods=['GET', 'POST'])
+@app.route('/switch_animation', methods=['POST'])
 def switch_animation():
+    """
+        Switches an animation
+    """
+
+    # Check format of POST data
     if "device_name" not in request.form:
         # TODO: log error
-        return
+        return "error: device name not specified"
     device_name = request.form["device_name"]
 
     if request.form["device_name"] not in app.data["output_devices"]:
