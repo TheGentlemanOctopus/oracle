e10b7ba8d55cac9ea58ef697f1ff22ec00dc594e
Aaron Fishman <uk.afishman@gmail.com>
2018-06-22 18:27:34 +0100

    comments

--- core/devices/wonderface_device.py
+++ core/devices/wonderface_device.py
@@ -8,15 +8,23 @@ from core.animations.spiral_out_fast import SpiralOutFast
 
 
 class WonderfaceDevice(OutputDevice):
+    """
+        A generic pixels by channel device that made be used for the wonderfields face
+        pixels_per_channel is a dict where key is channel and value is number of pixels
+        led_spacing and strip_spacing is only for im purposes
+        pixels are artificially placed as side-by-side strips channel-wise
+    """
     def __init__(self, led_spacing, strip_spacing, pixels_per_channel):
         super(WonderfaceDevice, self).__init__()
 
         self.pixels_by_channel = {}
 
+        # For placing strips
         start = np.array(([0,0,0])).astype(float)
         direction = np.array([1,0,0]).astype(float)
         strip_offset = np.array([0, strip_spacing, 0]).astype(float)
 
+        # Form one strip per channel
         pixels = []
         for channel, pixel_count in pixels_per_channel.items():
             strip = Strip(start, direction, led_spacing, pixel_count)
@@ -26,6 +34,6 @@ class WonderfaceDevice(OutputDevice):
 
             start += strip_offset
 
+        # A generic layout that can be used with generic patterns
         self.layout = PixelList(pixels)
-
         self.animation = SpiralOutFast(self.layout)
