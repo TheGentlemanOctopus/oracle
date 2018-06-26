cb6f60841afcf653e5906105ba0a67130f69963b
Aaron Fishman <uk.afishman@gmail.com>
2018-06-26 22:48:38 +0100

    switchin' em up

--- core/animations/carousel.py
+++ core/animations/carousel.py
@@ -2,13 +2,19 @@ from animation import Animation
 import utils
 
 import time
+import itertools
 import numpy as np
+
+default_set = [
+    {"type": "SwoopyTown"},
+    {"type": "SpiralOutFast"}
+]
 
 class Carousel(Animation):
     # TODO: only works on generic animations atm
     layout_type="Layout"
 
-    def __init__(self, layout, animations, transition_time=3):
+    def __init__(self, layout, animations=default_set, transition_time=3):
         """
             THE CAROUSEL
             goes round-and-round-and-round-and-round-and-round-and-round
@@ -26,27 +32,43 @@ class Carousel(Animation):
 
         self.next_switch = time.time() + transition_time
 
-        self.animation_dict = utils.possible_animations()
+        self.animation_dict = utils.possible_animations(layout.__class__.__name__)
+        if "Carousel" in self.animation_dict:
+            del self.animation_dict["Carousel"]
 
-        # Make sure animations are constructable to avoid disappointment later
-        self.animations = animations
-        for ani_data in animations:
-            if "name" not in ani_data:
-                raise Exception("name not defined in one of carousel's animations")
+        # round-and-round-and-round-and-round-and-round-and-round
+        self.animations = itertools.cycle(animations)
 
-            if ani_data["name"] not in animation_dict:
-                raise Exception("Unknown or incompatible animation %s"%ani_data["name"])
-            animation = self.animation_dict[name]
-
-            args = ani_data["args"] if "args" in ani_data else {}
-
-            # Test pattern can be constructed to avoid disappointment later
-            animation(layout, **args)
-
+        # Make sure all animations are constructable to avoid disappointment later
         self.current_animation = Animation()
+        for i in animations:
+            self.next_animation()
+
+    def next_animation(self):
+        """
+            Switches up the animation
+        """
+        ani_data = self.animations.next()
+
+        if "type" not in ani_data:
+            raise Exception("type not defined in one of carousel's animations")
+        ani_type = ani_data["type"]
+
+        if ani_data["type"] not in self.animation_dict:
+            raise Exception("Unknown or incompatible animation %s"%ani_data["type"])
+        args = ani_data["args"] if "args" in ani_data else {}
+
+        # Construct
+        self.current_animation = self.animation_dict[ani_type](self.layout, **args)
 
     def update(self):
         """
             Update-o
         """
-        pass
+        switch_time = self.params["transition_time"].value
+
+        if time.time() > self.next_switch:
+            self.next_switch += switch_time
+            self.next_animation()
+
+        self.current_animation.update()
