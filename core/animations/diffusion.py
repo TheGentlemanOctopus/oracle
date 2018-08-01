c27318f17f53479db0c8ee8ccfc0d0911b1a81eb
Aaron Fishman <uk.afishman@gmail.com>
2018-08-01 21:52:28 +0100

    diffuse the dews

--- core/animations/diffusion.py
+++ core/animations/diffusion.py
@@ -4,16 +4,66 @@ import numpy as np
 from scipy.spatial import Delaunay
 from sklearn.neighbors import kneighbors_graph
 import matplotlib.pyplot as plt
+import random
 
 class Diffusion(Animation):
-    def __init__(self, layout, num_neighbours=5):
+    def __init__(self, layout, num_neighbours=4):
         super(Diffusion, self).__init__()
         self.layout = layout
 
         points = np.array([p.location for p in self.layout.pixels])
 
         # TODO: I think triangulation would be better, this tends to give stitchy results
-        self.neighbours = kneighbors_graph(points, num_neighbours, mode='connectivity', include_self=False).toarray()
+        # Adjacency Matrix: https://en.wikipedia.org/wiki/Adjacency_matrix
+        self.points_graph = kneighbors_graph(points, num_neighbours, 
+            mode='connectivity', 
+            include_self=False
+        ).toarray()
+
+        self.pixels_np = np.array(self.pixels)
+
+        self.add_heat(200)
+        self.count = 0
+
+        self.blur_speed = 0.5
+
+    def add_heat(self, num_pixels):
+        # For dev purposes
+        start = random.choice(range(len(self.pixels)))
+        for pixel in self.pixels[start:start+num_pixels]:
+            pixel.r = random.random() 
+
+        # For dev purposes
+        # start = random.choice(range(len(self.pixels)))
+        # for pixel in self.pixels[start:start+num_pixels]:
+        #     pixel.g = random.random() 
+
+        # For dev purposes
+        start = random.choice(range(len(self.pixels)))
+        for pixel in self.pixels[start:start+num_pixels]:
+            pixel.b = random.random()   
 
     def update(self):
-        pass
+        self.count+=1
+        print "count", self.count
+
+        # Loop over each pixel
+        for i, pixel in enumerate(self.pixels):
+            # Neighbouring pixels
+            neighbour_indices = np.nonzero(self.points_graph[i])
+
+            blur_intensity = np.mean([p.r for p in self.pixels_np[neighbour_indices]])
+            pixel.r = self.blur_speed*blur_intensity + (1-self.blur_speed)*pixel.r
+
+            # blur_intensity = np.mean([p.g for p in self.pixels_np[neighbour_indices]])
+            # pixel.g = self.blur_speed*blur_intensity + (1-self.blur_speed)*pixel.g
+
+            blur_intensity = np.mean([p.b for p in self.pixels_np[neighbour_indices]])
+            pixel.b = self.blur_speed*blur_intensity + (1-self.blur_speed)*pixel.b
+
+
+
+        if self.count > 50:
+            self.add_heat(100)
+            self.count=0
+        # Average
