96b19d0a4badc50d008ad1c5b7833478a7dd2b95
Aaron Fishman <uk.afishman@gmail.com>
2018-08-01 23:08:11 +0100

    blur fun

--- core/animations/diffusion.py
+++ core/animations/diffusion.py
@@ -43,27 +43,31 @@ class Diffusion(Animation):
         for pixel in self.pixels[start:start+num_pixels]:
             pixel.b = random.random()   
 
-    def update(self):
-        self.count+=1
-        print "count", self.count
-
+    def blur(self):
+        """
+            Blur pixels by averaging over neighbouring pixel intensities for each color
+        """
         # Loop over each pixel
         for i, pixel in enumerate(self.pixels):
             # Neighbouring pixels
             neighbour_indices = np.nonzero(self.points_graph[i])
-
+    
+            # TODO: DRY this up
             blur_intensity = np.mean([p.r for p in self.pixels_np[neighbour_indices]])
             pixel.r = self.blur_speed*blur_intensity + (1-self.blur_speed)*pixel.r
 
-            # blur_intensity = np.mean([p.g for p in self.pixels_np[neighbour_indices]])
-            # pixel.g = self.blur_speed*blur_intensity + (1-self.blur_speed)*pixel.g
+            blur_intensity = np.mean([p.g for p in self.pixels_np[neighbour_indices]])
+            pixel.g = self.blur_speed*blur_intensity + (1-self.blur_speed)*pixel.g
 
             blur_intensity = np.mean([p.b for p in self.pixels_np[neighbour_indices]])
             pixel.b = self.blur_speed*blur_intensity + (1-self.blur_speed)*pixel.b
 
+    def update(self):
+        self.blur()
 
+        self.count+=1
+        print "count", self.count
 
         if self.count > 50:
             self.add_heat(100)
             self.count=0
-        # Average
