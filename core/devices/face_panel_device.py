f8bbbd997a31f08039ddf387f41ec4493ffe63be
Ben Holden <benholden7@gmail.com>
2018-08-03 00:13:00 +0100

    Creates strips in simulator, takes account for the channel length of beaglebone by adding dark space

--- core/devices/face_panel_device.py
+++ core/devices/face_panel_device.py
@@ -11,42 +11,41 @@ class FacePanelDevice(OutputDevice):
     """
         A generic pixels by channel device that made be used for displaying the individual panels as a series of strips
     """
-    def __init__(self, led_spacing, strip_spacing, channel):
+    def __init__(self, led_spacing, strip_spacing, channel, hw_chan_length):
         super(FacePanelDevice, self).__init__()
 
         self.pixels_by_channel = {}
 
         # For placing strips
-        start = np.array(([0,0,0])).astype(float)
+        start = np.array(([0,-3,0])).astype(float)
         direction = np.array([1,0,0]).astype(float)
         strip_offset = np.array([0, strip_spacing, 0]).astype(float)
 
         # Form one strip per face panel
         pixels = []
-        for pix_orders in fmap['right']:
-            pixel_count = pix_orders[1] - pix_orders[0]
-            strip = Strip(start, direction, led_spacing, pixel_count)
-
-            pixels.extend(strip.pixels)
+        for pix_orders in fmap['left']:
+            pixels.extend(gen_strip(pix_orders, start, direction, led_spacing))
             start += strip_offset
-
+        pixels.extend(gen_strip([fmap['stats']['l_pixels']+1, hw_chan_length], start, direction, led_spacing))
+        start += strip_offset
 
         for pix_orders in fmap['centre']:
-            pixel_count = pix_orders[1] - pix_orders[0]
-            strip = Strip(start, direction, led_spacing, pixel_count)
-
-            pixels.extend(strip.pixels)
+            pixels.extend(gen_strip(pix_orders, start, direction, led_spacing))
             start += strip_offset
-            
+        pixels.extend(gen_strip([fmap['stats']['c_pixels']+1, hw_chan_length], start, direction, led_spacing))
+        start += strip_offset
 
-        for pix_orders in fmap['left']:
-            pixel_count = pix_orders[1] - pix_orders[0]
-            strip = Strip(start, direction, led_spacing, pixel_count)
+        for pix_orders in fmap['right']:
+            pixels.extend(gen_strip(pix_orders, start, direction, led_spacing))
+            start += strip_offset
+        pixels.extend(gen_strip([fmap['stats']['r_pixels']+1, hw_chan_length], start, direction, led_spacing))
 
-            pixels.extend(strip.pixels)
-            start += strip_offset    
-        print pixels
         self.pixels_by_channel[int(channel)] = pixels
         # A generic layout that can be used with generic patterns
         self.layout = PixelList(pixels)
         self.animation = SpiralOutFast(self.layout)
+
+def gen_strip(pix_orders, start, direction, led_spacing):
+    pixel_count = (pix_orders[1] + 1) - pix_orders[0]
+    strip = Strip(start, direction, led_spacing, pixel_count)
+    return strip.pixels
