028aa9d3df1dffb3e69fb8f103ef1213ec87a97c
Aaron Fishman <uk.afishman@gmail.com>
2018-08-20 23:00:31 +0100

    wave purrrramz

--- core/animations/brendo.py
+++ core/animations/brendo.py
@@ -9,7 +9,14 @@ from numpy import pi
 class Brendo(Animation):
     layout_type = "Layout"
 
-    def __init__(self, layout, wavelength=1, amplitude=1, frequency=1):
+    def __init__(self, layout, 
+        wavelength=4, 
+        amplitude=0.21, 
+        frequency=2.6,
+        hue=0.58,
+        saturation=0.71,
+        fft_channel=0.9
+    ):
         """
             Shifts pixel colors along a hue range in the order that the led strips woulf be laid in
             period is the number of seconds it takes a color to lap the cube
@@ -20,8 +27,12 @@ class Brendo(Animation):
         self.layout = layout
 
         self.add_param("wavelength", wavelength, 0.5, 10)
-        self.add_param("amplitude", amplitude, 0.5, 10)
+        self.add_param("amplitude", amplitude, 0, 1)
         self.add_param("frequency", frequency, 0.5, 10)
+        self.add_param("saturation", saturation, 0, 1)
+        self.add_param("fft_channel", fft_channel, 0, 6)
+        
+        self.add_param("hue", hue, 0, 1)
 
     def update(self):
         pixels = self.layout.pixels
@@ -29,36 +40,18 @@ class Brendo(Animation):
         w = self.params["frequency"].value
         A = self.params["amplitude"].value
         l = self.params["wavelength"].value
+        sat = self.params["saturation"].value
+        fft_index = int(self.params["fft_channel"].value)
+
+        hue = self.params["hue"].value
         t = time.time()
 
         x = np.linspace(0, 1, len(pixels))
 
-        s = 1
+        s = sat + (1-sat)*self.fft[fft_index]
         v = 1
 
         for i, pixel in enumerate(pixels):
-            h = 2*A*np.sin(2*pi*x[i]/l)*np.cos(w*t)
+            h = (hue + A*np.sin(2*pi*x[i]/l)*np.cos(w*t)) % 1
 
             pixel.set_hsv(h,s,v)
-
-        return
-
-        period = self.params["period"].value
-        hue_range = self.params["hue_range"].value
-
-        # Shift pixel ordering according to period phase
-        shift = int(len(pixels)*(time.time() % period)/period)
-        pixels = np.concatenate([pixels[shift:], pixels[:shift]])
-
-        # One lap of the color wheel in the order
-        s = 1 # Saturation
-        v = 1 # Value
-        for i, h in enumerate(np.linspace(0, hue_range, len(pixels))):
-            
-            # shift hue with weighted fft avg that favours bass :)
-            weights = np.array(range(len(self.fft)))[::-1]
-            h_shift = np.average(self.fft, weights=weights)
-            h_shifted = (h + h_shift) % 1
-
-            pixel = pixels[i]
-            pixel.r, pixel.g, pixel.b = colorsys.hsv_to_rgb(h_shifted,s,v)
